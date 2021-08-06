06fa0c7eb833e5b0505c1d8da0b9e269a6befb71
outeruse2010 <outeruse2010@gmail.com>
2021-08-06 12:11:50 +0800

    Malini UI - customer screen started

--- python/malini/malini-ui/src/components/content/customer/Customers.jsx
+++ python/malini/malini-ui/src/components/content/customer/Customers.jsx
@@ -9,3 +9,131 @@ const Customers = () => {
 }
 
 export default Customers
+
+import React, {useState, useEffect} from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+import {Button, IconButton, Tooltip, Typography, Box}  from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+
+import {customer_atom, act_customer_atom, fetch_customer_areas, delete_customer} from './customer_api';
+
+import {useRecoilState, useRecoilValue} from 'recoil';
+import { dialog_atom } from '../utils/DialogComp';
+import DialogComp from '../utils/DialogComp';
+import { message_atom } from '../utils/SnakbarComp';
+import SnakbarComp from '../utils/SnakbarComp';
+
+const Customers = () => {
+
+    const [customer_list, setcustomer_list] = useRecoilState(customer_atom);
+    const [act_customer_res, setAct_customer_res] = useRecoilState(act_customer_atom);
+    const [dialog_message, setDialog_message] = useRecoilState(dialog_atom);
+    const [act_message, setAct_message] = useRecoilState(message_atom);
+
+
+    useEffect(() => {
+            const input = {user:"Test"};
+            const customer_res = fetch_customer_areas(input);
+            customer_res.then(data => setcustomer_list(data));
+        }, []);
+
+    const [openAreaModal, setOpenAreaModal] = useState(false);
+    const [selected_area, setSelected_area] = useState(null);
+
+    const toggleAreaModal = () => {        
+        setOpenAreaModal(!openAreaModal);
+    };
+
+    const onAddNewClick = () => {
+        setSelected_area(null);
+        toggleAreaModal();
+    }
+
+    const onDeleteClick = (row) => {
+        setSelected_area(row);
+        let title = 'Delete Customer Area';
+        let content = 'Are you sure to DELETE area [' + row['area_name'] + '] ?';
+        setDialog_message({title, content});
+    };
+
+    const onEditClick = (row) => {
+        setSelected_area(row);
+        setOpenAreaModal(true);
+    };
+
+    const onDialogClose = (ans) => {
+        if(ans === 'Y'){
+            let area_id = selected_area['area_id'];
+            let area_name = selected_area['area_name'];
+
+            let customer_json = {area_id, area_name, updated_by: 'Test'};
+
+            const res = delete_customer(customer_json);
+            res.then(data => {
+                setAct_customer_res(data);
+                if(data.status === 'success'){
+                    const input = {user:"Test"};
+                    const customer_res = fetch_customer_areas(input);
+                    customer_res.then(customers => setcustomer_list(customers));
+                }            
+                setAct_message(data);
+            });
+        }
+    };
+     
+
+    const renderEditButton = (params) => {
+        return (            
+            <IconButton onClick={() => {onEditClick(params.row);}}>
+                <Tooltip title="Edit" arrow><EditIcon fontSize="small"/></Tooltip>
+            </IconButton>
+        );
+    }
+
+    const renderDeleteButton = (params) => {
+        return (            
+            <IconButton onClick={() => {onDeleteClick(params.row) }}>
+                <Tooltip title="Delete" arrow><DeleteIcon fontSize="small"/></Tooltip>
+            </IconButton>
+        );
+    }
+
+    const rows = useRecoilValue(customer_atom);
+
+    const columns = [
+        { field: "area_id", headerName: "Edit", renderCell: renderEditButton ,  width: 105}
+        ,{ field: "", headerName: "Delete", renderCell: renderDeleteButton,  width: 120 }
+        ,{ field: 'area_name', headerName: 'Area', width: 180 }
+        ,{ field: 'description', headerName: 'Description', width: 300 }
+        ,{ field: 'created_by', headerName: 'Created By', width: 200 }
+        ,{ field: 'created_on', headerName: 'Created On', width: 150 }
+        ,{ field: 'updated_by', headerName: 'Updated By', width: 200 }
+        ,{ field: 'updated_on', headerName: 'Updated On', width: 160 }
+        ];
+
+    return (
+        <div>
+            <Box display='flex' p={1} >
+                <Box p={1} flexGrow={1}><Typography variant="h6" noWrap > Customer Areas </Typography></Box>
+                <Box p={1}>
+                <Button type="button" onClick={onAddNewClick} size="small" color="primary" variant="outlined"> Add New Area</Button>
+                </Box>
+            </Box>
+
+            <div style={{ height: 500, width: '100%' }}>
+                <DataGrid rows={rows} columns={columns}   disableSelectionOnClick rowsPerPageOptions={[]}/>
+            </div>
+
+            <CustomerAreaEntry selected_area={selected_area} openAreaModal={openAreaModal} toggleAreaModal={toggleAreaModal} />
+            <DialogComp onDialogClose={(ans)=> onDialogClose(ans)}/>
+            <SnakbarComp />
+        </div>
+    );
+}
+
+export default Customers;
+
+
+
+
--- python/malini/malini-ui/src/components/content/customer/customer_api.js
+++ python/malini/malini-ui/src/components/content/customer/customer_api.js
@@ -26,3 +26,32 @@ export const delete_cus_area = async (cus_area_json) => {
     const data = res.json();   
     return data;
 }
+
+
+/***************************************  CUSTOMER api  *****************************/
+
+export const customer_atom = atom({key: "customer_atom", default: []});
+export const act_customer_atom = atom({key: "act_customer_atom", default: {} });
+
+export const fetch_customers = async (input = {}) => {
+    const req = post_request('fetch_customers', input);
+    const res = await fetch(req);
+    const data = res.json();    
+    return data;
+};
+
+export const add_update_customer = async (customer_json) => {
+    const api = customer_json['cus_id'] ? 'update_customer' : 'add_customer';
+    const req = post_request(api, customer_json);
+    const res = await fetch(req);
+    const data = res.json();   
+    return data;
+}
+
+export const delete_customer = async (customer_json) => {
+    const api = 'remove_customer';
+    const req = post_request(api, customer_json);
+    const res = await fetch(req);
+    const data = res.json();   
+    return data;
+}
